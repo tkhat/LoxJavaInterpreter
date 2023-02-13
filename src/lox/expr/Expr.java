4b9db20915aece8045c4329069898e8839a0b7ce
tkhat <temurkhatiashvili@gmail.com>
2023-02-13 23:31:48 +0400

    add some util function to map source code to java actual objects

---
+++ src/lox/expr/Expr.java
@@ -0,0 +1,20 @@
+package expr;
+
+
+import token.Token;
+
+public abstract class Expr {
+    static class Binary extends Expr {
+
+
+
+        Binary(Expr left, Token operator, Expr right) {
+            this.left = left;
+            this.operator = operator;
+            this.right = right;
+        }
+        final Expr left;
+        final Token operator;
+        final Expr right;
+    }
+}
